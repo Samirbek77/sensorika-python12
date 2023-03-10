c75491eabcd6fd197d2a3ebb129ef019c132cf38
Samirbek Gulomov <122879617+Samirbek77@users.noreply.github.com>
2023-03-10 15:13:41 +0500

    Add files via upload

---
+++ uyga.py
@@ -0,0 +1,22 @@
+firstname = "Samirbek"
+lastname = "Gulomov"
+age = 15
+email = "gulomovsamirbek@gmail.com"
+full_info = f"Salom. Mening ismim {firstname}.Familiyam esa {lastname}.Yoshim {age} da. Elektron pochta manzilim: {email} "
+print(full_info)
+
+
+#Mexanizatsiyalashtirishmoqchiliginiaytganinisezibqoldimmikana ) 
+print("Mexanizatsiya".upper())
+print("Mexanizatsiya".lower())
+print("aytganini sezib qoldimikana".capitalize())
+print("aytganini sezib qoldimikana".title())
+print("aytganini sezib qoldimikana".split(" "))
+print("      Mexanizatsiya".lstrip())
+print("Mexanizatsiya       ".rstrip())
+print("     Mexanizatsiya    ".strip())
+print(" ".zfill(12))
+print("•".join("Python").upper())
+print("Mexanizatsiyalashtirilmagan".count("m"))
+print("Mexanizatsiyalashtirilmagan".index("n"))
+print(len("Mexanizatsiyalashtirilmaganidandir"))	
